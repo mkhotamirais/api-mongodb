73854c55b72a9f45a5af018f427f5547f2557ccd
Mkhotamirais <mkhotamirais@gmail.com>
2024-08-22 20:52:28 +0700

    multer masalah

--- v4/router.js
+++ v4/router.js
@@ -1,9 +1,9 @@
 import express from "express";
 const router = express.Router();
 import { createProduct, deleteProduct, readProductById, readProducts, updateProduct } from "./productController.js";
-import upload from "./multer.js";
+// import upload from "./multer.js";
 
-router.route("/product").get(readProducts).post(upload, createProduct);
-router.route("/product/:id").get(readProductById).patch(upload, updateProduct).delete(deleteProduct);
+router.route("/product").get(readProducts).post(createProduct);
+router.route("/product/:id").get(readProductById).patch(updateProduct).delete(deleteProduct);
 
 export default router;
